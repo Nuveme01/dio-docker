5cf88c7318423ec72fdd4f0371c6bf10bf563ce6
Nuveme01 <nuveme03@gmail.com>
2025-04-26 10:07:19 -0300

    Adicionada primay key e ajustado nome das colunas para FN

---
+++ banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dados (
+    aluno_id int NOT NULL PRIMARY KEY,
+    nome varchar(50),
+    sobrenome varchar(50),
+    endereco varchar(150),
+    cidade varchar(50),
+    host varchar(50)
+);
